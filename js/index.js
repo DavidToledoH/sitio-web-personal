361debdec1229e7d66f386a0c253dce456ddb14b
DavidToledoH <55214027+DavidToledoH@users.noreply.github.com>
2022-05-01 23:48:01 -0500

    Base de la pagina

---
+++ js/index.js
@@ -0,0 +1,8 @@
+(function($){
+    $(function(){
+      $('.sidenav').sidenav();
+      $('.pushpin').pushpin();
+      $('.parallax').parallax();
+      $('.collapsible').collapsible();
+    }); 
+  })(jQuery); 
